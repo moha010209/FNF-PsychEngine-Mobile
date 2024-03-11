deb651fedb47ab0ec8edd56e0648560033b4db8e
moha010209 <162739798+moha010209@users.noreply.github.com>
2024-03-11 15:13:46 +0100

    remove these also for more based app

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -42,7 +42,6 @@ import shaders.CustomShaders;
 
 import objects.Note.EventNote;
 import objects.*;
-import states.stages.objects.*;
 
 #if LUA_ALLOWED
 import psychlua.*;
@@ -397,14 +396,6 @@ class PlayState extends MusicBeatState
 		switch (curStage)
 		{
 			case 'stage': new states.stages.StageWeek1(); //Week 1
-			case 'spooky': new states.stages.Spooky(); //Week 2
-			case 'philly': new states.stages.Philly(); //Week 3
-			case 'limo': new states.stages.Limo(); //Week 4
-			case 'mall': new states.stages.Mall(); //Week 5 - Cocoa, Eggnog
-			case 'mallEvil': new states.stages.MallEvil(); //Week 5 - Winter Horrorland
-			case 'school': new states.stages.School(); //Week 6 - Senpai, Roses
-			case 'schoolEvil': new states.stages.SchoolEvil(); //Week 6 - Thorns
-			case 'tank': new states.stages.Tank(); //Week 7 - Ugh, Guns, Stress
 		}
 
 		if(isPixelStage) {
